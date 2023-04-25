7a8e9d91287f52045becbd08f47ccaac31e5ce58
Vela <davidbak38@gmail.com>
2023-04-25 16:19:30 +0200

    Merge création de la première page

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<?php
+require("./assets/php/ManagerGames.php");
+$managerGame = new ManagerGames();
+$allGames = $managerGame->getAll();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +14,27 @@
   <link rel="stylesheet" href="./assets/css/style.css">
   <title>Document</title>
 </head>
+
 <body>
-  <?php 
-  include ("./assets/nav-bar/nav_bar.php")
+  <?php
+  include("./assets/nav-bar/nav_bar.php");
   ?>
+  <section class="game-container">
+    <h1>Exemple de titre</h1>
+    <div class="display">
+      <div class="table-container">
+        <?php
+        include("./assets/php/listGames.php")
+        ?>
+      </div>
+      <div class="form-container">
+        <?php
+        include("./assets/php/formGames.php")
+        ?>
+      </div>
+    </div>
+  </section>
   <script src="./assets/js/script.js" type="module"></script>
 </body>
+
 </html>
